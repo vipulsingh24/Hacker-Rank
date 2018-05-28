dc158135a647d7ef44e7b9ee18122b73ffa18fee
vipul <letsmailvipul@gmail.com>
2018-05-28 22:13:33 +0530

    .

--- Python/10 Days of Statistics/Binomial_Distribution_2.py
+++ Python/10 Days of Statistics/Binomial_Distribution_2.py
@@ -1,3 +1,10 @@
+'''
+A manufacturer of metal pistons finds that, on average, 12% of the pistons they manufacture are rejected because they are incorrectly sized. What is the probability that a batch of 10 pistons will contain:
+
+1. No more than 2 rejects?
+2. At least 2 rejects?
+'''
+
 def factorial(n):
     if n == 0:
         return 1
