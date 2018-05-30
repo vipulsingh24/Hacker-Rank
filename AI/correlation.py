8181627da617a9866360a6e7df0f7080ec8d21f4
vipul <letsmailvipul@gmail.com>
2018-05-30 16:43:36 +0530

    .

--- AI/correlation.py
+++ AI/correlation.py
@@ -14,7 +14,7 @@ sum_x = sum_list(x)
 sum_y = sum_list(y)
 sum_xy = sum_list([a * b for a, b in zip(x, y)])
 sum_x2 = sum_list([a**2 for a in x]) # Summation x^2
-sum_y2 = sum_list([a**2 for a in y]) # Summation y^2
+sum_y2 = sum_list([a**2 for a in y]) # Summation y^2	
 
 num = (n * sum_xy) - (sum_x * sum_y)
 denom = (((n*sum_x2 - (sum_x)**2)) * ((n*sum_y2 - (sum_y)**2)))**(1/2)
---
+++ Python/10 Days of Statistics/Pearson Correlation Coefficient.py
@@ -0,0 +1,32 @@
+def mean(a, n):
+    result = 0
+    for i in a:
+        result += i
+    result /= n
+    return round(result, 3)
+
+def std_dev(a, n):
+    m = mean(a, n)
+    result = 0
+    for i in a:
+        result += (i - m) ** 2
+    result = (result / n) ** (0.5)
+    return round(result, 3)
+
+def cov(x, y, x_m, y_m, n):
+    result = 0
+    for i in range(n):
+        x[i] -= x_m
+        y[i] -= y_m
+    for i in range(n):
+        result +=  (x[i] * y[i])
+    return result / n
+
+n = int(input())
+x = [float(x) for x in input().split()]
+y = [float(x) for x in input().split()]
+x_m = mean(x, n)
+y_m = mean(y, n)
+num = cov(x, y, x_m, y_m, n)
+denom = std_dev(x, n) * std_dev(y, n)
+print(round((num / denom), 3))
